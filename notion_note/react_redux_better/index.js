8554a5707dc68075b9878014e99f82b5603fcc91
birdsen <72699487+birdsen@users.noreply.github.com>
2022-10-14 21:53:38 +0800

    react-redux

--- notion_note/react_redux_better/index.js
+++
@@ -1,7 +0,0 @@
-// react项目的入口文件
-
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from './App'
-
-ReactDom.render(<App />, document.getElementById('root'))
--- notion_note/react_redux_better/redux/count/action.js
+++
@@ -1,5 +0,0 @@
-import { INCREMENT, DECREMENT } from '../constants'
-
-// 事实上，通常会将 reducer 和 action 合并到一起
-export const createIncrementAction = data => ({ type: INCREMENT, data }) // 圆括号是为了返回对象
-export const createDecrementAction = data => ({ type: DECREMENT, data })
